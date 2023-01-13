14a443359151d58ccece0eed53a9aa9b8446b58b
vishnu <vishnu@rhombuz.io>
2023-01-13 23:26:06 +0530

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.lightBlue[200],
+              child: Text('Chart!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TRANSACTION'),
+          )
+        ],
+      ),
+    );
+  }
+}
